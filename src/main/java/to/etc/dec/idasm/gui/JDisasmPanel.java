2cc4407bf69be1d11bb307c20decb1194167a808
Frits Jalvingh <jal@etc.to>
2024-12-22 22:21:05 +0100

    Handle partial rendering of panel.

--- src/main/java/to/etc/dec/idasm/gui/JDisasmPanel.java
+++ src/main/java/to/etc/dec/idasm/gui/JDisasmPanel.java
@@ -7,6 +7,7 @@ import to.etc.dec.idasm.disassembler.pdp11.IByteSource;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class JDisasmPanel extends JPanel implements Scrollable {
 	private final IByteSource m_source;
@@ -18,23 +19,41 @@ public class JDisasmPanel extends JPanel implements Scrollable {
 	 */
 	private int[] m_lineAddresses = new int[8192];
 
+	/**
+	 * Y positions per line, as calculated.
+	 */
+	private int[] m_posMap = new int[8192];
+
 	private int m_lineCount;
 
 	private DisContext m_context;
 
 	private final Font m_font = new Font("Arial", Font.PLAIN, 14);
 
+	private int m_yPos;
+
+	private int m_fontHeight;
+
+	private int m_panelHeight;
+
 
 	private boolean m_initialized;
 
-	public JDisasmPanel(IByteSource source, IDisassembler disassembler) {
+	private int m_maxAscent;
+
+	public JDisasmPanel(IByteSource source, IDisassembler disassembler) throws Exception {
 		m_source = source;
 		m_disassembler = disassembler;
 		setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	}
 
 	@Override public Dimension getPreferredSize() {
-		return new Dimension(1024, 8192);
+		try {
+			initialize();
+		} catch(Exception x) {
+			x.printStackTrace();
+		}
+		return new Dimension(1024, m_panelHeight);
 	}
 
 	@Override public void paintComponent(Graphics g) {
@@ -46,21 +65,31 @@ public class JDisasmPanel extends JPanel implements Scrollable {
 			g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
 			g2d.setFont(m_font);
 
-			if(!m_initialized) {
-				initialize(g);
-				g.setColor(Color.WHITE);
-				//g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
-				m_initialized = true;
+			int fromY = g.getClipBounds().y;
+			System.out.println(
+				"repaint: "
+					+ g.getClipBounds().width
+					+ " x " + g.getClipBounds().height
+					+ " @ " + g.getClipBounds().x + ", " + g.getClipBounds().y
+			);
+
+			//-- Calculate the closest Y line
+			int index = Arrays.binarySearch(m_posMap, 0, m_lineCount, fromY);
+			if(index < 0) {
+				index = -(index + 1);
 			}
+			if(index > 0)
+				index--;						// Get one line before
+			m_yPos = m_posMap[index];			// That will render here,
+			int endY = fromY + g.getClipBounds().height;
+			int addr = m_lineAddresses[index];
 
-			//-- Calculate the region
-			m_yPos = 0;
-			int line = 0;
-
-			int addr = m_lineAddresses[line];
+			System.out.println("- index=" + index + ", ypos=" + m_yPos + ", addr=" + Integer.toOctalString(addr));
 			m_context.setCurrentAddress(addr);
-			while(m_yPos < g.getClip().getBounds().height) {
-				renderLine(g, line++);
+			while(m_yPos < endY) {
+				System.out.println("-- renderLine @" + m_yPos + ", addr=" + Integer.toOctalString(m_context.getCurrentAddress()));
+				renderLine(g, index);
+				index++;
 			}
 		} catch(Exception x) {
 			x.printStackTrace();
@@ -72,38 +101,49 @@ public class JDisasmPanel extends JPanel implements Scrollable {
 		m_disassembler.disassemble(m_context);
 
 		//-- Start rendering
-		g.drawString(m_context.getAddressString(), 10, m_yPos);
+		g.drawString(m_context.getAddressString(), 10, m_yPos + m_maxAscent);
 
 		m_yPos += m_fontHeight;
 	}
 
-	private int m_yPos;
+	private void initialize() throws Exception {
+		if(!m_initialized) {
+			Graphics g = getGraphics();
+			Graphics2D g2d= (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+			g2d.setFont(m_font);
 
-	private int m_fontHeight;
+			FontMetrics fontMetrics = g.getFontMetrics();
 
-	private int m_panelHeight;
-
-	private void initialize(Graphics g) throws Exception {
-		FontMetrics fontMetrics = g.getFontMetrics();
-
-		m_fontHeight = fontMetrics.getHeight();
-		m_panelHeight = 0;
-		m_yPos = 0;
-		m_context = DisassemblerMain.disassemble(m_disassembler, m_source, 036352, m_source.getEndAddress(), a -> {
-			addLine(a.getStartAddress());
-
-		});
-		m_panelHeight = m_yPos;
-		//setSize(1024, m_panelHeight);
+			m_fontHeight = fontMetrics.getHeight();
+			m_maxAscent = fontMetrics.getMaxAscent();
+			m_panelHeight = 0;
+			m_yPos = 0;
+			m_context = DisassemblerMain.disassemble(m_disassembler, m_source, 036352, m_source.getEndAddress(), a -> {
+				addLine(a.getStartAddress(), m_yPos);
+				m_yPos += m_fontHeight;
+			});
+			m_panelHeight = m_yPos;
+			//setSize(1024, m_panelHeight);
+			m_initialized = true;
+		}
 	}
 
-	private void addLine(int address) {
+	private void addLine(int address, int yPos) {
 		if(m_lineCount >= m_lineAddresses.length) {
 			int[] li = new int[m_lineAddresses.length * 2];
 			System.arraycopy(m_lineAddresses, 0, li, 0, m_lineAddresses.length);
 			m_lineAddresses = li;
+
+			li = new int[m_posMap.length * 2];
+			System.arraycopy(m_posMap, 0, li, 0, m_posMap.length);
+			m_posMap = li;
 		}
-		m_lineAddresses[m_lineCount++] = address;
+		m_lineAddresses[m_lineCount] = address;
+		m_posMap[m_lineCount] = yPos;
+		m_lineCount++;
 	}
 
 
